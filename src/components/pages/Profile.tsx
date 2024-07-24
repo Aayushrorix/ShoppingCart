1656ab4d667658301fc34e19a6b31d24b1ba7b01
Aayushrorix <aayushg.rorix@gmail.com>
2024-07-24 18:18:07 +0530

    chnage foir profile page

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,10 +1,21 @@
 // import React from 'react'
 
+import { useEffect } from "react"
 import EditProfile from "../EditProfile"
 import Header from "../Header"
+import { useNavigate } from "react-router-dom"
 // import Products from "../Products"
 
 function Profile() {
+
+  const token = localStorage.getItem('token')
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/login')
+    }
+  },[token])
   return (
     <>
             <Header/>
